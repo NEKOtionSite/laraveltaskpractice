a20183c45a4f01219ec61a4a38133e2fe940b246
jbenola <102546460+NEKOtionSite@users.noreply.github.com>
2023-03-06 12:13:42 +0800

    create a laravel task practice folder

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+@extends('tasks.layout')
+@section('content')
+<div class="card">
+  <div class="card-header">Task Page</div>
+  <div class="card-body">
+  
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $tasks->name }}</h5>
+      
+  </div>
+      
+    </hr>
+  
+  </div>
+</div>
