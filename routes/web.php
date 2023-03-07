7af464ac65ae1defaabeaed2565c7bb98faec55e
unknown <juliebenola730@gmail.com>
2023-03-07 21:12:22 +0800

    Update context route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
- 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
- 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource("/task", TaskController::class);
+
+Route::get('/', [TaskController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/task', [TaskController::class, 'store']);
+Route::delete('/task/{task}', [TaskController::class, 'destroy']);
