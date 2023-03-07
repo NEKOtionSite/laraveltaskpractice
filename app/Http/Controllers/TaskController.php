cfa6e771c67ebea3cba3138353cc9dc92eb161c2
unknown <juliebenola730@gmail.com>
2023-03-07 17:08:45 +0800

    edit context inside  taskcontroller on app/http/controllers folder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,62 +1,35 @@
 <?php
- 
 namespace App\Http\Controllers;
- 
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-
 use App\Models\Task;
-use Illuminate\View\View;
- 
+use Illuminate\Support\Facades\Validator;
 class TaskController extends Controller
 {
- 
-    public function index(): View
-    {
-        $tasks = Task::all();
-        return view ('tasks.index')->with('tasks', $tasks);
+    public function index()
+{
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+}
+public function store(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
     }
- 
-    public function create(): View
-    {
-        return view('tasks.create');
-    }
- 
-  
-    public function store(Request $request): RedirectResponse
-    {
-        $input = $request->all();
-        Task::create($input);
-        return redirect('task')->with('flash_message', 'Task Addedd!');
-    }
- 
-    public function show(string $id): View
-    {
-        $task = Task::find($id);
-        return view('tasks.show')->with('tasks', $task);
-    }
- 
-    public function edit(string $id): View
-    {
-        $task = Task::find($id);
-        return view('tasks.edit')->with('tasks', $task);
-    }
- 
-    public function update(Request $request, string $id): RedirectResponse
-    {
-        $task = Task::find($id);
-        $input = $request->all();
-        $task->update($input);
-        return redirect('task')->with('flash_message', 'task Updated!');  
-    }
- 
-    
-    public function destroy(string $id): RedirectResponse
-    {
-        Task::destroy($id);
-        return redirect('task')->with('flash_message', 'Task deleted!');
-   
-    }  
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+}
+public function destroy(Task $task)
+{
+    $task->delete();
+    return redirect('/');
+}
 }
